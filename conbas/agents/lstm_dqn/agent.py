73949884af42c78e54a5a821b16c5993a95c1c2e
Harald Reisinger <ha.reisinger@gmail.com>
2021-04-17 12:18:26 +0200

    Refactor Transiation, Memory classes form agent to core file
    Rename my_utils which fix problem with unused parent core/utils files by renaming
    those, need to fix this later

--- conbas/agents/lstm_dqn/agent.py
+++ conbas/agents/lstm_dqn/agent.py
@@ -1,6 +1,5 @@
-from typing import NamedTuple, Optional, List, Dict, Any, Tuple, Callable
+from typing import Optional, List, Dict, Any, Tuple, Callable
 import math
-import random
 from pathlib import Path
 from collections import deque
 import time
@@ -19,96 +18,8 @@ from textworld import EnvInfos
 
 from model import LstmDqnModel
 from policy import LinearAnnealedEpsGreedyQPolicy
-from my_utils import preprocess, words_to_ids
-
-
-class Transition(NamedTuple):
-    observation: List[int]
-    command_index: torch.Tensor
-    reward: float
-    next_observation: List[int]
-    done: bool
-
-
-class TransitionBatch(NamedTuple):
-    observation: List[List[int]]
-    command_index: List[torch.Tensor]
-    reward: List[float]
-    next_observation: List[List[int]]
-    done: List[bool]
-
-
-class Memory:
-    def __init__(self, batch_size: int) -> None:
-        self.batch_size = batch_size
-
-    def sample(self, replace: bool = False) -> List[Transition]:
-        raise NotImplementedError
-
-    def __len__(self) -> int:
-        raise NotImplementedError
-
-
-class ReplayMemory(Memory):
-    def __init__(self, capacity: int, batch_size: int) -> None:
-        super().__init__(batch_size)
-
-        self.capacity = capacity
-        self.memory = []
-        self.pos = 0
-
-    def append(self, transition: Transition) -> None:
-        if len(self) < self.capacity:
-            self.memory.append(transition)
-        else:
-            self.memory[self.pos] = transition
-        self.pos = (self.pos + 1) % self.capacity
-
-    def sample(self, replace: bool = False) -> List[Transition]:
-        indices = np.random.choice(
-            np.arange(len(self)), size=self.batch_size, replace=replace)
-        return [self.memory[i] for i in indices]
-
-    def __len__(self) -> int:
-        return len(self.memory)
-
-
-# TODO impl from microsft start code, read paper and reimplement it
-class PrioritizedReplayMemory:
-    def __init__(self, capacity: int, priority_fraction: float = 0.0) -> None:
-        self.priority_fraction = priority_fraction
-        self.alpha_capacity = int(capacity * priority_fraction)
-        self.beta_capacity = capacity - self.alpha_capacity
-        self.alpha_memory, self.beta_memory = [], []
-        self.alpha_position, self.beta_position = 0, 0
-
-    def append(self, transition: Transition, is_prior: bool = False) -> None:
-        if self.priority_fraction == 0.0:
-            is_prior = False
-        if is_prior:
-            if len(self.alpha_memory) < self.alpha_capacity:
-                self.alpha_memory.append(None)
-            self.alpha_memory[self.alpha_position] = transition
-            self.alpha_position = (self.alpha_position + 1) % self.alpha_capacity
-        else:
-            if len(self.beta_memory) < self.beta_capacity:
-                self.beta_memory.append(None)
-            self.beta_memory[self.beta_position] = transition
-            self.beta_position = (self.beta_position + 1) % self.beta_capacity
-
-    def sample(self, batch_size: int) -> List[Transition]:
-        if self.priority_fraction == 0.0:
-            from_beta = min(batch_size, len(self.beta_memory))
-            res = random.sample(self.beta_memory, from_beta)
-        else:
-            from_alpha = min(int(self.priority_fraction * batch_size), len(self.alpha_memory))
-            from_beta = min(batch_size - int(self.priority_fraction * batch_size), len(self.beta_memory))
-            res = random.sample(self.alpha_memory, from_alpha) + random.sample(self.beta_memory, from_beta)
-        random.shuffle(res)
-        return res
-
-    def __len__(self):
-        return len(self.alpha_memory) + len(self.beta_memory)
+from utils import preprocess, words_to_ids
+from core import Memory, ReplayMemory, Transition, TransitionBatch
 
 
 class LstmDqnAgent:
@@ -172,7 +83,7 @@ class LstmDqnAgent:
             obs: List that contains the current observation (=feedback) for each game
             infos: additional (step) information for each game
 
-        returns:
+        Returns:
             input_tensor: tensor of shape (max_len, batch_size) containing the padded state information
                 for each game in the batch.
                 max_len: maximum length of game information
---
+++ conbas/agents/lstm_dqn/core.py
@@ -0,0 +1,94 @@
+from typing import List, NamedTuple
+import random
+
+import numpy as np
+import torch
+
+
+class Transition(NamedTuple):
+    observation: List[int]
+    command_index: torch.Tensor
+    reward: float
+    next_observation: List[int]
+    done: bool
+
+
+class TransitionBatch(NamedTuple):
+    observation: List[List[int]]
+    command_index: List[torch.Tensor]
+    reward: List[float]
+    next_observation: List[List[int]]
+    done: List[bool]
+
+
+class Memory:
+    def __init__(self, batch_size: int) -> None:
+        self.batch_size = batch_size
+
+    def sample(self, replace: bool = False) -> List[Transition]:
+        raise NotImplementedError
+
+    def __len__(self) -> int:
+        raise NotImplementedError
+
+
+class ReplayMemory(Memory):
+    def __init__(self, capacity: int, batch_size: int) -> None:
+        super().__init__(batch_size)
+
+        self.capacity = capacity
+        self.memory = []
+        self.pos = 0
+
+    def append(self, transition: Transition) -> None:
+        if len(self) < self.capacity:
+            self.memory.append(transition)
+        else:
+            self.memory[self.pos] = transition
+        self.pos = (self.pos + 1) % self.capacity
+
+    def sample(self, replace: bool = False) -> List[Transition]:
+        indices = np.random.choice(
+            np.arange(len(self)), size=self.batch_size, replace=replace)
+        return [self.memory[i] for i in indices]
+
+    def __len__(self) -> int:
+        return len(self.memory)
+
+
+# TODO impl from microsft start code, read paper and reimplement it
+class PrioritizedReplayMemory:
+    def __init__(self, capacity: int, priority_fraction: float = 0.0) -> None:
+        self.priority_fraction = priority_fraction
+        self.alpha_capacity = int(capacity * priority_fraction)
+        self.beta_capacity = capacity - self.alpha_capacity
+        self.alpha_memory, self.beta_memory = [], []
+        self.alpha_position, self.beta_position = 0, 0
+
+    def append(self, transition: Transition, is_prior: bool = False) -> None:
+        if self.priority_fraction == 0.0:
+            is_prior = False
+        if is_prior:
+            if len(self.alpha_memory) < self.alpha_capacity:
+                self.alpha_memory.append(None)
+            self.alpha_memory[self.alpha_position] = transition
+            self.alpha_position = (self.alpha_position + 1) % self.alpha_capacity
+        else:
+            if len(self.beta_memory) < self.beta_capacity:
+                self.beta_memory.append(None)
+            self.beta_memory[self.beta_position] = transition
+            self.beta_position = (self.beta_position + 1) % self.beta_capacity
+
+    def sample(self, batch_size: int) -> List[Transition]:
+        if self.priority_fraction == 0.0:
+            from_beta = min(batch_size, len(self.beta_memory))
+            res = random.sample(self.beta_memory, from_beta)
+        else:
+            from_alpha = min(int(self.priority_fraction * batch_size), len(self.alpha_memory))
+            from_beta = min(batch_size - int(self.priority_fraction * batch_size), len(self.beta_memory))
+            res = random.sample(self.alpha_memory, from_alpha) + random.sample(self.beta_memory, from_beta)
+        random.shuffle(res)
+        return res
+
+    def __len__(self):
+        return len(self.alpha_memory) + len(self.beta_memory)
