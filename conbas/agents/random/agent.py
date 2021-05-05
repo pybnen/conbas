7875e712b8631ce870c7e44697a7393998870d64
Harald Reisinger <ha.reisinger@gmail.com>
2021-05-05 15:11:14 +0200

    Add random agent

---
+++ conbas/agents/random/agent.py
@@ -0,0 +1,29 @@
+from typing import Optional, List, Dict, Any
+import numpy as np
+
+from textworld import EnvInfos
+
+
+class RandomAgent:
+
+    def __init__(self, commands: Optional[List[str]] = None, use_admissible=False) -> None:
+        assert commands or use_admissible
+        
+        self.commands = commands
+        self.use_admissible = use_admissible
+
+    def request_infos(self) -> Optional[EnvInfos]:
+        """Request the infos the agent expects from the environment
+
+        Returns:
+            request_infos: EnvInfos"""
+        request_infos = EnvInfos()
+        request_infos.admissible_commands = self.use_admissible
+        return request_infos
+
+    def act(self, obs: List[str], infos: Dict[str, List[Any]]
+            ) -> List[str]:
+        if self.use_admissible:
+            return [str(np.random.choice(commands)) for commands in infos["admissible_commands"]]
+        else:
+            return np.random.choice(self.commands, size=len(obs)).tolist()
