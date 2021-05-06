fe2a3b347ac65da60a6a8ede3fc5965a0b50ee16
Harald Reisinger <ha.reisinger@gmail.com>
2021-05-06 15:39:10 +0200

    Add option to evaluate random agent with commands list

--- conbas/apps/eval.py
+++ conbas/apps/eval.py
@@ -13,26 +13,47 @@ import gym
 from conbas.agents import RandomAgent, LstmDqnAgent
 
 
-def build_parser():
+def parse_arguments():
     description = "Evaluate agent on a textworld environment"
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("game_file")
+    parser.add_argument("--repeat", type=int, default=100)
     parser.add_argument("--agent", choices=['random', 'lstm_dqn'], required=True)
-    parser.add_argument("--ckpt-path", type=str)
-    parser.add_argument("--max-steps", type=int, default=0, metavar="STEPS",
-                        help="Limit maximum number of steps.")
-    return parser
+    parser.add_argument("--max-steps", type=int, default=50, metavar="STEPS",
+                        help="Limit maximum number of steps. (0: unlimited)")
+
+    agent_parser = {}
+    agent_parser["random"] = argparse.ArgumentParser(description="Arguments for random agent.")
+    agent_parser["random"].add_argument("--commands-file", type=str)
+
+    agent_parser["lstm_dqn"] = argparse.ArgumentParser(description="Arguments for lstm_dqn agent.")
+    agent_parser["lstm_dqn"].add_argument("--ckpt-path", type=str, required=True)
+
+    args, extras = parser.parse_known_args()
+    # add agent arguments to parser arguments
+    agent_parser[args.agent].parse_args(extras, namespace=args)
+
+    return args
 
 
-def get_random_agent():
+def get_random_agent(args):
     def act(agent, obs, infos):
         return agent.act(obs, infos)
 
-    print("Evaluate RandomAgent")
-    return RandomAgent(use_admissible=True), act
+    print("Evaluate RandomAgent with arguments {}".format(args))
+    if args.commands_file is not None:
+        from conbas.agents.lstm_dqn.train import get_commands
+        commands_files = [args.commands_file]
+        commands = get_commands(commands_files)
+
+        agent = RandomAgent(commands=commands)
+    else:
+        agent = RandomAgent(use_admissible=True)
+
+    return agent, act
 
 
-def get_lstm_dqn_agent(ckpt_path):
+def get_lstm_dqn_agent(args):
     from conbas.agents.lstm_dqn.train import get_commands, get_word_vocab
     from conbas.agents.lstm_dqn.policy import EpsGreedyQPolicy
 
@@ -41,10 +62,10 @@ def get_lstm_dqn_agent(ckpt_path):
             commands, _, _ = agent.act(obs, infos)
         return commands
 
-    ckpt = torch.load(ckpt_path)
-    config = ckpt["config"]
+    print("Evaluate LstmDqnAgent with arguments {}".format(args))
 
-    print("Evaluate LstmDqnAgent")
+    ckpt = torch.load(args.ckpt_path)
+    config = ckpt["config"]
     print(yaml.dump(config))
 
     commands_files = config["general"]["commands_files"]
@@ -55,19 +76,18 @@ def get_lstm_dqn_agent(ckpt_path):
 
     agent = LstmDqnAgent(config, commands, word_vocab)
     agent.policy = EpsGreedyQPolicy(0.1, agent.device)
-    agent.load_state_dict(ckpt_path, "state_dict")
+    agent.load_state_dict(args.ckpt_path, "state_dict")
     agent.lstm_dqn.eval()
     return agent, act
 
 
 def main():
-    args = build_parser().parse_args()
+    args = parse_arguments()
 
     if args.agent == "random":
-        agent, act = get_random_agent()
+        agent, act = get_random_agent(args)
     elif args.agent == "lstm_dqn":
-        assert args.ckpt_path is not None
-        agent, act = get_lstm_dqn_agent(args.ckpt_path)
+        agent, act = get_lstm_dqn_agent(args)
     else:
         # TODO raise exception
         assert False
@@ -85,7 +105,7 @@ def main():
     max_score = infos["max_score"][0]
     scores_arr = []
 
-    with tqdm(range(100)) as pbar:
+    with tqdm(range(args.repeat)) as pbar:
         for _ in pbar:
             obs, infos = env.reset()
             agent.init(obs, infos)
